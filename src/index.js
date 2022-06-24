10bf5199ea78ff4b399aa7e6247cae7806232b48
Sandy <phbloomwood@gmail.com>
2022-06-24 12:57:55 -0400

    Added the city in the function
    HTML : added a space between the 2 spans in the h1
    JS : Changed the function name to add the city and created a city variable to fetch the city from axios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-function displayPromptCityTemp(response) {
+/**** Display the h1 temperature ***/
+
+function displayCityAndTemp(response) {
   console.log(response);
+
+  let city = document.querySelector("#city-title");
+  city.innerHTML = response.data.name;
+
   let cityTemp = document.querySelector("#cityTemp");
   cityTemp.innerHTML = Math.round(response.data.main.temp);
 }
 
-//let cityName = prompt("Tell me a city");
 let key = "59ebc73950183d72b027190e832e1b5b";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${key}&units=metric`;
 
-axios.get(url).then(displayPromptCityTemp);
+axios.get(url).then(displayCityAndTemp);
+
+/**** Display the forecast temperatures ***/
+/* 
+function displayForecast(response) {
+  console.log(response);
+  //console.log(response.data.list["1"]);
+}
+
+let apiKey = "59ebc73950183d72b027190e832e1b5b";
+let forecastUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=3&appid=${apiKey}`;
+
+axios.get(forecastUrl).then(displayForecast);
+*/
