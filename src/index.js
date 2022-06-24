a89b975a6e9cd5bfa4db92ebd22fc73e27dceea2
Sandy <phbloomwood@gmail.com>
2022-06-24 12:26:33 -0400

    Added span around city and temperature in HTML and created the axios call and function in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function displayPromptCityTemp(response) {
+  console.log(response);
+  let cityTemp = document.querySelector("#cityTemp");
+  cityTemp.innerHTML = Math.round(response.data.main.temp);
+}
 
+//let cityName = prompt("Tell me a city");
+let key = "59ebc73950183d72b027190e832e1b5b";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${key}&units=metric`;
+
+axios.get(url).then(displayPromptCityTemp);
