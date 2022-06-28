3d90ff840d1593fa59ad6f27e155ad1b2d7d76d4
Sandy <phbloomwood@gmail.com>
2022-06-28 09:04:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,6 +52,7 @@ function displayCityAndTemp(response) {
 }
 
 let key = "59ebc73950183d72b027190e832e1b5b";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${key}&units=metric`;
+let city = "Montreal";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
 axios.get(url).then(displayCityAndTemp);
